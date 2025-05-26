99acf4ad02a3ee61e63dc4de39156f5d6e610092
bruno Salas <65439485+BrunoSalas@users.noreply.github.com>
2025-05-26 16:51:01 -0500

    Tema 7
    Falta las misiones

--- Assets/Scripts/SoupLetter.cs
+++ Assets/Scripts/SoupLetter.cs
@@ -1,18 +1,51 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using TMPro;
 using UnityEngine;
 
 public class SoupLetter : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public TMP_InputField inputField;
+    public string correct;
+    public GameObject input;
+    public Animator DoorGood;
+    public Animator DoorBad;
+    public GameObject correctGameobject;
+    public GameObject badGameobject;
 
-    // Update is called once per frame
-    void Update()
+    public void InputFieldIntroduce()
     {
-        
+        string inputText = Regex.Replace(inputField.text, @"\s+", "").Trim(); // Elimina espacios internos y externos
+        string correctText = Regex.Replace(correct, @"\s+", "").Trim(); // Hace lo mismo con el texto correcto
+        Debug.LogError($"inputField: [{inputText}] length: {inputText.Length}");
+        Debug.LogError($"correct: [{correctText}] length: {correctText.Length}");
+
+        if (string.Equals(inputText, correctText, StringComparison.OrdinalIgnoreCase))
+        {
+            Debug.LogError("A");
+            DoorGood.SetBool("OpenBool", true);
+            badGameobject.SetActive(false);
+            correctGameobject.SetActive(false);
+            correctGameobject.SetActive(true);
+        }
+        else
+        {
+            DoorBad.SetBool("OpenBool", true);
+            badGameobject.SetActive(false);
+            correctGameobject.SetActive(false);
+            badGameobject.SetActive(true);
+        }
+
+        input.SetActive(false);
+    }
+    public void InputFieldSalir()
+    {
+        input.SetActive(false);
+    }
+    public void InputFieldAparecer()
+    {
+        input.SetActive(true);
     }
 }
