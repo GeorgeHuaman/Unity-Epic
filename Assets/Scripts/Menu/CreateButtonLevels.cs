766a09343ca34b8f55a8f5941010db4e160584d0
George Jimy <knivespeans@gmail.com>
2025-05-26 18:10:28 -0500

    Generar selector de temas y versiones

---
+++ Assets/Scripts/Menu/CreateButtonLevels.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class CreateButtonLevels : MonoBehaviour
+{
+    public List<LevelData> levelsData = new List<LevelData>();
+    public GameObject parent;
+    public GameObject prefab;
+    void Start()
+    {
+        foreach (LevelData levelData in levelsData)
+        {
+            GameObject go = Instantiate(prefab, parent.transform);
+            for (int i = 0; i <= 4; i++)
+            {
+                if (i == 0)
+                {
+                    RectTransform child = go.GetComponentInChildren<RectTransform>();
+                    TMP_Text temaText = child.GetComponentInChildren<TMP_Text>();
+                    temaText.text = levelData.name;
+                }
+                else
+                {
+                    int index = i; // Necesario para capturar correctamente el índice en la lambda
+                    Transform child = go.transform.GetChild(index);
+                    child.GetComponent<Button>().onClick.AddListener(() => {
+                        LoadScene(levelData.versions[index - 1].versionName); // -1 porque el primer hijo (i==0) era el título
+                    });
+                }
+            }
+        }
+    }
+
+    void LoadScene(string nameScene)
+    {
+        SceneManager.LoadScene(nameScene);
+    }
+}
