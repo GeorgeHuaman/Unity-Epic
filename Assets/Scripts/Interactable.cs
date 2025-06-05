11028e2cac73ca21ec2d63c1e873948ae86cb755
George Jimy <knivespeans@gmail.com>
2025-06-05 12:41:49 -0500

    Avance Misiones Tema 5, retoques Script Interactable, Orden Tema 4

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -1,22 +1,19 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Xml;
-using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class Interactable : MonoBehaviour
 {
-    [ReadOnly]public string interactableID;
+    [ReadOnly] public string interactableID;
     public float interactableRadius;
     public float interactableRadiusVisibility;
-    [HideInInspector]public GameObject player;
-    [HideInInspector]public bool inputButton;
+    [HideInInspector] public GameObject player;
+    [HideInInspector] public bool inputButton;
     public Animator animator;
     public GameObject button;
     private float distance;
+
     public enum type
     {
         None,
@@ -25,50 +22,61 @@ public class Interactable : MonoBehaviour
         Type3,
         Type4,
     }
+
     public type iconType;
 
     [Header("Unity Events")]
-    public UnityEvent unityEvent;
+    public UnityEvent onEnterEvent;
+    public UnityEvent onInteractEvent;
+    public UnityEvent onExitEvent;
 
     [Header("Quest Event")]
     public UnityEvent questEvent;
 
-    // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindWithTag("Player");
         AssignEvent();
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (RangeVisibility())
         {
-            if(button)
-            button.SetActive(true);
+            if (button)
+                button.SetActive(true);
             Vector3 lookDir = Camera.main.transform.position - transform.position;
-            lookDir.y = 0f; // Solo gira en el plano XZ
+            lookDir.y = 0f;
             if (button)
                 button.transform.forward = -lookDir;
         }
-        else
-            if (button)
-            button.SetActive(false);
-
-        if(RangePlayer())
+        else if (button)
         {
-            if(Input.GetKeyDown(KeyCode.F) || inputButton)
-                 EnterEvent();
+            button.SetActive(false);
+        }
+
+        if (RangePlayer())
+        {
+            if (Input.GetKeyDown(KeyCode.F) || inputButton)
+                EnterEvent();
         }
     }
 
     public void EnterEvent()
     {
         inputButton = false;
-        unityEvent?.Invoke();
-        //questEvent.Invoke();
+        StartCoroutine(HandleInteraction());
     }
+
+    private IEnumerator HandleInteraction()
+    {
+        onEnterEvent?.Invoke();
+        yield return null;
+        onInteractEvent?.Invoke();
+        yield return null;
+        onExitEvent?.Invoke();
+    }
+
     public void SetAnimatorBoolTrue(string parameterName)
     {
         Animator animator = this.animator;
@@ -90,25 +98,24 @@ public class Interactable : MonoBehaviour
     public bool RangePlayer()
     {
         Vector3 horizontalDistance = new Vector3(
-            this.transform.position.x - player.transform.position.x,
+            transform.position.x - player.transform.position.x,
             0,
-            this.transform.position.z - player.transform.position.z
+            transform.position.z - player.transform.position.z
         );
 
         float distance = horizontalDistance.magnitude;
-
         return distance <= interactableRadius;
     }
+
     public bool RangeVisibility()
     {
         Vector3 horizontalDistance = new Vector3(
-            this.transform.position.x - player.transform.position.x,
+            transform.position.x - player.transform.position.x,
             0,
-            this.transform.position.z - player.transform.position.z
+            transform.position.z - player.transform.position.z
         );
 
         float distance = horizontalDistance.magnitude;
-
         return distance <= interactableRadiusVisibility;
     }
 
@@ -119,10 +126,8 @@ public class Interactable : MonoBehaviour
 
     private void AssignID()
     {
-        // Solo si no tiene ya un ID
         if (!string.IsNullOrEmpty(interactableID)) return;
 
-        // Obtener todos los objetos con este script
         Interactable[] all = FindObjectsOfType<Interactable>();
         HashSet<string> existingIDs = new HashSet<string>();
         foreach (var s in all)
@@ -131,7 +136,6 @@ public class Interactable : MonoBehaviour
                 existingIDs.Add(s.interactableID);
         }
 
-        // Buscar el primer ID disponible
         int id = 0;
         while (existingIDs.Contains(id.ToString("D2")))
         {
@@ -141,6 +145,7 @@ public class Interactable : MonoBehaviour
         interactableID = id.ToString("D2");
         Debug.Log($"Asignado ID {interactableID} a {gameObject.name}", gameObject);
     }
+
     void AssignEvent()
     {
         Transform parent = transform.parent;
@@ -149,9 +154,8 @@ public class Interactable : MonoBehaviour
             ImageInteractuable imageInteractuable = parent.GetComponent<ImageInteractuable>();
             if (imageInteractuable != null)
             {
-                unityEvent.AddListener(imageInteractuable.OpenUI);
+                onInteractEvent.AddListener(imageInteractuable.OpenUI);
             }
         }
     }
 }
-
