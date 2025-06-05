ec5e4127bbe6853b25f52d352b88e41e2b399eb8
bruno Salas <65439485+BrunoSalas@users.noreply.github.com>
2025-06-04 21:02:50 -0500

    Avances del tema 8, 7 y tema 6 terminado

---
+++ Assets/Scripts/Return.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Return : MonoBehaviour
+{
+    public Vector3 init;
+    void Start()
+    {
+        init = transform.position;
+    }
+}
